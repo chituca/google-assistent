f416d0703f9ce9b5a51e2dc97d5673ae97796a02
chituca <31902882+chituca@users.noreply.github.com>
2018-05-09 22:35:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/caixaWebhook", function(req, res) {
       ? req.body.queryResult.parameters.jogo
       : "Tivemos algum problema, pode repetir, por favor?";
   return res.json({   
-          "fulfillmentText": "Caixa Response",
+          "fulfillmentText": "06 25 26 35 38 40 \n Estimativa de prêmio do próximo concurso, em 10/05/2018, é de R$ 30.000.000,00 \n Acumulado para o próximo concurso R$ 26.744.923,97",
           "fulfillmentMessages": [{
             "text": {"text":["06 25 26 35 38 40 \n Estimativa de prêmio do próximo concurso, em 10/05/2018, é de R$ 30.000.000,00 \n Acumulado para o próximo concurso R$ 26.744.923,97"]}
           }
