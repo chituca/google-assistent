44e5e32a51ff8cec16ebd92e21668560d6c195ef
chituca <31902882+chituca@users.noreply.github.com>
2018-05-09 21:37:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,17 +22,17 @@ restService.post("/caixaWebhook", function(req, res) {
       : "Tivemos algum problema, pode repetir, por favor?";
   return res.json({   
 "fulfillmentText": "Caixa Response",
-"fulfillmentMessages": [
+"fulfillmentMessages": [{
     "card": {
       "title": "Caixa",
-      "subtitle": "card text",
+      "subtitle": "Caixa Econômica Federal",
       "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
       "buttons": [
         {
           "text": "button text",
           "postback": "https://assistant.google.com/"
         }
-      ]
+        }]
     }
 ],
 "source": "caixa.gov.br",
