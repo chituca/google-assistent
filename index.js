cf5a6b90d19dfd5c882ec97563528866c70223d5
chituca <31902882+chituca@users.noreply.github.com>
2018-05-11 16:49:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ restService.post("/caixaWebhook", function(req, res) {
       ? req.body.queryResult.parameters.jogo
       : "Tivemos algum problema, pode repetir, por favor?";
   return res.json({   
-          "fulfillmentText": "<speak>" + "ok <break time=\"2s\"/>, os números sorteados foram:"+ 
+          "fulfillmentText": "<speak>" + 
+          "ok <break time=\"2s\"/>, os números sorteados foram:" +
+	        "06 <say-as interpret-as="cardinal">12 22 28 31 44</say-as>, \n a estimativa de prêmio para o próximo concurso," +
+	        "em 12/05/2018, é de R$ 50.000.000,00, \n o valor acumulado para o próximo concurso é de R$ 44.786.421,27" + 
           "</speak>",
           "fulfillmentMessages": [{
             "text": {"text":["agora esse?"+" mesmo"]}
