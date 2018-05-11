48bc15aa94b696b1068e959dbdb0ec6368cea194
chituca <31902882+chituca@users.noreply.github.com>
2018-05-11 16:56:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ restService.post("/caixaWebhook", function(req, res) {
       : "Tivemos algum problema, pode repetir, por favor?";
   return res.json({   
           "fulfillmentText": "<speak>" + 
-          "ok <break time=\"2s\"/>, os números sorteados foram:" +
-          "06 <say-as interpret-as=\"cardinal\">12</say-as>, \n a estimativa de prêmio para o próximo concurso," +
-	        "em 12/05/2018, é de R$ 50.000.000,00, \n o valor acumulado para o próximo concurso é de R$ 44.786.421,27" + 
+          "ok <break time=\"1s\"/>, os números sorteados foram: " +
+          "06 <say-as interpret-as=\"cardinal\">12</say-as> <say-as interpret-as=\"cardinal\">22</say-as>"+
+	  "<say-as interpret-as=\"cardinal\">28</say-as> <say-as interpret-as=\"cardinal\">31</say-as>"+
+	  "<say-as interpret-as=\"cardinal\">31</say-as> <say-as interpret-as=\"cardinal\">44</say-as>, <break time=\"1s\"/>"+
+	  "\n a estimativa de prêmio para o próximo concurso," +
+	  "em 12/05/2018, é de R$ 50.000.000,00, \n o valor acumulado para o próximo concurso é de R$ 44.786.421,27" + 
           "</speak>",
           "fulfillmentMessages": [{
             "text": {"text":["agora esse?"+" mesmo"]}
