b096850039c0155945a0026e7e6ef90622964375
chituca <31902882+chituca@users.noreply.github.com>
2018-05-16 00:34:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const https = require('https');
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+var retorno = '';
 
 app.use(
     bodyParser.urlencoded({
@@ -15,21 +16,24 @@ app.use(
 app.use(bodyParser.json());
 
 app.post("/caixaWebhook", function(req, res) {
-    var speech =
+    var jogo =
       req.body.queryResult &&
       req.body.queryResult.parameters &&
       req.body.queryResult.parameters.jogo
         ? req.body.queryResult.parameters.jogo
-        : "Tivemos algum problema, pode repetir, por favor?";
+        : "Erro";
+    if(jogo = "Mega-Sena"){
+        retorno = "<speak>" + 
+        "ok <break time=\"1s\"/>, os números sorteados para "+jogo+" foram: " +
+        "06 <say-as interpret-as=\"cardinal\">12</say-as> <say-as interpret-as=\"cardinal\">22</say-as>"+
+          "<say-as interpret-as=\"cardinal\">28</say-as> <say-as interpret-as=\"cardinal\">31</say-as>"+
+          "<say-as interpret-as=\"cardinal\">44</say-as><break time=\"1s\"/>"+
+          "\n a estimativa de prêmio para o próximo concurso, " +
+          "em 12/05/2018, é de R$ 50.000.000,00, \n <break time=\"1s\"/>o valor acumulado para o próximo concurso é de R$ 44.786.421,27" + 
+        "</speak>";
+    }
     return res.json({   
-            "fulfillmentText": "<speak>" + 
-            "ok <break time=\"1s\"/>, os números sorteados para "+speech+" foram: " +
-            "06 <say-as interpret-as=\"cardinal\">12</say-as> <say-as interpret-as=\"cardinal\">22</say-as>"+
-              "<say-as interpret-as=\"cardinal\">28</say-as> <say-as interpret-as=\"cardinal\">31</say-as>"+
-              "<say-as interpret-as=\"cardinal\">44</say-as><break time=\"1s\"/>"+
-              "\n a estimativa de prêmio para o próximo concurso, " +
-              "em 12/05/2018, é de R$ 50.000.000,00, \n <break time=\"1s\"/>o valor acumulado para o próximo concurso é de R$ 44.786.421,27" + 
-            "</speak>",
+            "fulfillmentText": retorno,
             "fulfillmentMessages": [{
               "text": {"text":["Assistente Virtual Caixa!"]}
             }
@@ -59,7 +63,7 @@ function getLoteria(options, cb){
 }
 
 //*** Chama API de Loterias ***//
-/*var options = {
+var options = {
     host: 'api.caixa.gov.br',
     port: 8443,
     path: '/loterias/v2/resultados/Mega-Sena?concurso=',
@@ -73,7 +77,7 @@ getLoteria(options, function(err, result){
         reject();
     }
     console.log(result);
-});*/
+});
 
 app.listen(process.env.PORT || 8000, function() {
     console.log("Caixa server google assistente rodando!");
