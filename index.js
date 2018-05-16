07d685de8e1e86be9b78cfec0ad4a724a428954f
chituca <31902882+chituca@users.noreply.github.com>
2018-05-15 23:48:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,80 @@
+'use strict';
+
 const http = require('http');
+const https = require('https');
 const express = require("express");
 const bodyParser = require("body-parser");
-const restService = express();
-const host = 'viacep.com.br';
-const wwoApiKey = '';
+const app = express();
 
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+  );
 
-restService.use(bodyParser.json());
+app.use(bodyParser.json());
 
-restService.post("/caixaWebhook", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.jogo
-      ? req.body.queryResult.parameters.jogo
-      : "Tivemos algum problema, pode repetir, por favor?";
-var jogo = '01001000';
-//*** chama API de loterias ***//
-  getLoterias(jogo).then((output) => {
-    res.json({ 'fulfillmentText': output,
-    "fulfillmentMessages": [{
-        "text": {"text":["Assistente Virtual Caixa!"]}
-      }
-    ],
-    "source": "caixa.gov.br" }); // Return the results of the weather API to Dialogflow
-  }).catch(() => {
-    res.json({ 'fulfillmentText': `I don't know the weather but I hope it's good!` });
-  });
-});
-
-function getLoterias (jogo) {
-  return new Promise((resolve, reject) => {
-    // Cria o path para HTTP request 
-    let path = '/ws/' + jogo + 'json/';
-    console.log('API Request: ' + host + path);
-
-    // Make the HTTP request to get jogo
-    http.get({host: host, path: path}, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        let cep = response['cep'];
-        let location = response['localidade'];
-        let conditions = response['ibge'];
-        let currentConditions = response['bairro'];
-
-        // Create response
-        let output = `dados da consulta de CEP ${cep} ,
-        ${location} - ${currentConditions} ${currentConditions}.`;
-
-        // Resolve the promise with the output text
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        console.log(`Error ao chamar API de loterias: ${error}`)
-        reject();
-      });
+app.post("/caixaWebhook", function(req, res) {
+    var speech =
+      req.body.queryResult &&
+      req.body.queryResult.parameters &&
+      req.body.queryResult.parameters.jogo
+        ? req.body.queryResult.parameters.jogo
+        : "Tivemos algum problema, pode repetir, por favor?";
+    return res.json({   
+            "fulfillmentText": "<speak>" + 
+            "ok <break time=\"1s\"/>, os números sorteados para "+speech+" foram: " +
+            "06 <say-as interpret-as=\"cardinal\">12</say-as> <say-as interpret-as=\"cardinal\">22</say-as>"+
+              "<say-as interpret-as=\"cardinal\">28</say-as> <say-as interpret-as=\"cardinal\">31</say-as>"+
+              "<say-as interpret-as=\"cardinal\">44</say-as><break time=\"1s\"/>"+
+              "\n a estimativa de prêmio para o próximo concurso, " +
+              "em 12/05/2018, é de R$ 50.000.000,00, \n <break time=\"1s\"/>o valor acumulado para o próximo concurso é de R$ 44.786.421,27" + 
+            "</speak>",
+            "fulfillmentMessages": [{
+              "text": {"text":["Assistente Virtual Caixa!"]}
+            }
+  
+          ],
+          "source": "caixa.gov.br"
     });
-
   });
+
+function getLoteria(options, cb){
+
+    https.request(options, function(res){
+        var body = '';
+    
+        res.on('data', function(chunk){
+            body +=chunk;
+        });
+    
+        res.on('end', function(){
+            var result = JSON.parse(body);
+            cb(null, result);
+        });
+        res.on('error', cb);
+    })
+    .on('err', cb)
+    .end();
 }
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
+
+//*** Chama API de Loterias ***//
+var options = {
+    host: 'api.caixa.gov.br',
+    port: 8443,
+    path: '/loterias/v2/resultados/Mega-Sena?concurso=',
+    method: 'GET',
+    rejectUnauthorized: false
+};
+
+getLoteria(options, function(err, result){
+    if(err){
+        return console.log('Error ao acessar a API: ', err);
+        reject();
+    }
+    console.log(result);
 });
+
+/*app.listen(process.env.PORT || 8000, function() {
+    console.log("Caixa server google assistente rodando!");
+  });*/
