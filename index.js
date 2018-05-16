9ba1e0a215b11b69ec7a3ab9e83c8c8a4692725e
chituca <31902882+chituca@users.noreply.github.com>
2018-05-16 00:00:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,68 @@
-'use strict';
+"use strict";
 
-const http = require('http');
-const https = require('https');
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
 
-app.use(
-    bodyParser.urlencoded({
-      extended: true
-    })
-  );
+const restService = express();
 
-app.use(bodyParser.json());
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.post("/caixaWebhook", function(req, res) {
-    var speech =
-      req.body.queryResult &&
-      req.body.queryResult.parameters &&
-      req.body.queryResult.parameters.jogo
-        ? req.body.queryResult.parameters.jogo
-        : "Tivemos algum problema, pode repetir, por favor?";
-    return res.json({   
-            "fulfillmentText": "<speak>" + 
-            "ok <break time=\"1s\"/>, os números sorteados para "+speech+" foram: " +
-            "06 <say-as interpret-as=\"cardinal\">12</say-as> <say-as interpret-as=\"cardinal\">22</say-as>"+
-              "<say-as interpret-as=\"cardinal\">28</say-as> <say-as interpret-as=\"cardinal\">31</say-as>"+
-              "<say-as interpret-as=\"cardinal\">44</say-as><break time=\"1s\"/>"+
-              "\n a estimativa de prêmio para o próximo concurso, " +
-              "em 12/05/2018, é de R$ 50.000.000,00, \n <break time=\"1s\"/>o valor acumulado para o próximo concurso é de R$ 44.786.421,27" + 
-            "</speak>",
-            "fulfillmentMessages": [{
-              "text": {"text":["Assistente Virtual Caixa!"]}
-            }
-  
-          ],
-          "source": "caixa.gov.br"
-    });
+restService.use(bodyParser.json());
+
+restService.post("/caixaWebhook", function(req, res) {
+  var speech =
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.jogo
+      ? req.body.queryResult.parameters.jogo
+      : "Tivemos algum problema, pode repetir, por favor?";
+  return res.json({   
+          "fulfillmentText": "<speak>" + 
+          "ok <break time=\"1s\"/>, os números sorteados foram: " +
+          "06 <say-as interpret-as=\"cardinal\">12</say-as> <say-as interpret-as=\"cardinal\">22</say-as>"+
+	        "<say-as interpret-as=\"cardinal\">28</say-as> <say-as interpret-as=\"cardinal\">31</say-as>"+
+	        "<say-as interpret-as=\"cardinal\">44</say-as><break time=\"1s\"/>"+
+	        "\n a estimativa de prêmio para o próximo concurso, " +
+	        "em 12/05/2018, é de R$ 50.000.000,00, \n <break time=\"1s\"/>o valor acumulado para o próximo concurso é de R$ 44.786.421,27" + 
+          "</speak>",
+          "fulfillmentMessages": [{
+            "text": {"text":["Assistente Virtual Caixa!"]}
+          }
+
+        ],
+        "source": "caixa.gov.br"
   });
+});
 
+//*** Chamada API de Loterias ***//
+/*var apiKey = '';
+var result ='';
+
+function cb (err, response, body){
+  if(err){
+    console.log('error', error);
+  }
+  var loterias = JSON.parse(body);
+  if(loterias.message == 'jogo não localizado'){
+    result = 'Não foi possível localizar o jogo!';
+  } else {
+    result= 'o resultado foi:';
+  }
+}
+
+function getLoterias(jogo){
+  result = undefined;
+  var url = 'https://api.caixa.gov.br:8443/loterias/v2/resultados/Mega-Sena?concurso=2018';
+  var req = request(url,cb);
+  while (result === undefined){
+    require('deasync').runLoopOnce();
+    }
+    return result;
+}*/
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
