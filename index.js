6230b5145faccd5a464c9c5913bf04b28f3873fc
chituca <31902882+chituca@users.noreply.github.com>
2018-05-17 20:36:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,36 @@ app.post("/caixaWebhook", function(req, res) {
        let retorno;
        switch(loteriaSelecionada){
            case loterias.Mega:
-                getMegaSena(result);
+           let concurso = result.resultado.concurso;
+           let ganhadores = result.resultado.ganhadores;
+           let sorteados = result.resultado.resultado.split('-').sort();
+           let dataSorteio = formata_data(result.resultado.data);
+           let dataProximo = formata_data(result.resultado.DT_PROXIMO_CONCURSO);
+           let cabecalho = "<speak>ok <break time=\"1s\"/>, para o concurso "+concurso+" foram sorteados: " +
+           "<say-as interpret-as=\"cardinal\">"+sorteados[0]+"</say-as>,"+
+           "<say-as interpret-as=\"cardinal\">"+sorteados[1]+"</say-as>,"+
+           "<say-as interpret-as=\"cardinal\">"+sorteados[2]+"</say-as>,"+
+           "<say-as interpret-as=\"cardinal\">"+sorteados[3]+"</say-as>,"+
+           "<say-as interpret-as=\"cardinal\">"+sorteados[4]+"</say-as> e"+
+           "<say-as interpret-as=\"cardinal\">"+sorteados[5]+"</say-as>,";
+       
+           if(ganhadores === 0) {
+               var estimativa = formataReal(result.resultado.VR_ESTIMATIVA);
+               var acumulado = formataReal(result.resultado.valor_acumulado1);
+               retorno = cabecalho+"<break time=\"1s\"/>o prêmio acumulou e a estimativa para o próximo concurso, em "+dataProximo+
+               ", é de "+ estimativa + " <break time=\"1s\"/>, o valor acumulado para o próximo concurso é de "+acumulado+".</speak>";
+           
+               } else {
+               var premio = formataReal(result.resultado.valor);
+               var apostasTexto = '';
+                   if(ganhadores > 1){
+                       apostasTexto = "apostas foram premiadas";
+                   } else {
+                       apostasTexto = "aposta foi premiada";
+                   }
+               retorno = cabecalho+"<break time=\"1s\"/> <say-as interpret-as=\"cardinal\">"+ganhadores+
+               "</say-as>"+apostasTexto+" com valor de "+premio+"</speak>";    
+               }
                 break;
             case loterias.Quina:      
                 retorno = "aqui bateu na Quina.";
