aa1ac99ba11858564ca64f399a206596464820e1
Washington Medeiros <31902882+chituca@users.noreply.github.com>
2018-05-18 11:12:35 -0300

    Update util.js

--- util.js
+++ util.js
@@ -1,7 +1,6 @@
-var exports = modeule.exports = {};
-
-exports.formataReal =  function(n) {
-    var valor = parseFloat(n);
+module.exports = {
+    functionformataReal: function(n) {
+        var valor = parseFloat(n);
     n = String(valor.toFixed(2));
     var v = n.replace(/\D/g, "").replace(/(\d{2})$/, ",$1").replace(/(\d+)(\d{3},\d{2})$/g, "$1.$2");
     var qtdLoop = (v.length - 3) / 3;
@@ -11,9 +10,9 @@ exports.formataReal =  function(n) {
         v = v.replace(/(\d+)(\d{3}.*)/, "$1.$2");
     }
     return "R$ " + v;
-}
-    
-exports.formataData = function(str) {
-     var data_siopi = str.replace(/[^\d]+/g, '');
+    },
+    formataData: function(){
+        var data_siopi = str.replace(/[^\d]+/g, '');
      return data_siopi.substr(6,2)+"/"+data_siopi.substr(4,2)+"/"+data_siopi.substr(0,4);
     }
+}
