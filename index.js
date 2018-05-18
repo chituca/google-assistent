eaebff4156854b1eabd0b8c5023d11139b434f01
chituca <31902882+chituca@users.noreply.github.com>
2018-05-18 10:07:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var util = require("util");
 const http = require('http');
 const https = require('https');
 const express = require("express");
@@ -41,7 +42,7 @@ app.post("/caixaWebhook", function(req, res) {
                 getMegaSena(result);
                 break;
             case loterias.Quina:      
-                retorno = "aqui bateu na Quina.";
+                getQuina(result);
                 break;
 
                 default:
@@ -96,6 +97,52 @@ function getMegaSena(result) {
     let concurso = result.resultado.concurso;
     let ganhadores = result.resultado.ganhadores;
     let sorteados = result.resultado.resultado.split('-').sort();
+    let dataSorteio = util.formataData(result.resultado.data);
+    let dataProximo = formata_data(result.resultado.DT_PROXIMO_CONCURSO);
+    let cabecalho = "<speak>ok <break time=\"1s\"/>, para o concurso "+concurso+" foram sorteados: " +
+    "<say-as interpret-as=\"cardinal\">"+sorteados[0]+"</say-as>,"+
+    "<say-as interpret-as=\"cardinal\">"+sorteados[1]+"</say-as>,"+
+    "<say-as interpret-as=\"cardinal\">"+sorteados[2]+"</say-as>,"+
+    "<say-as interpret-as=\"cardinal\">"+sorteados[3]+"</say-as>,"+
+    "<say-as interpret-as=\"cardinal\">"+sorteados[4]+"</say-as> e"+
+    "<say-as interpret-as=\"cardinal\">"+sorteados[5]+"</say-as>,";
+
+    if(ganhadores === 0) {
+        var estimativa = formataReal(result.resultado.VR_ESTIMATIVA);
+        var acumulado = formataReal(result.resultado.valor_acumulado1);
+        retorno = cabecalho+"<break time=\"1s\"/>o prêmio acumulou e a estimativa para o próximo concurso, em "+dataProximo+
+        ", é de "+ estimativa + " <break time=\"1s\"/>, o valor acumulado para o próximo concurso é de "+acumulado+".</speak>";
+    
+        } else {
+        var premio = formataReal(result.resultado.valor);
+        var apostasTexto = '';
+            if(ganhadores > 1){
+                apostasTexto = "apostas foram premiadas";
+            } else {
+                apostasTexto = "aposta foi premiada";
+            }
+        retorno = cabecalho+"<break time=\"1s\"/> <say-as interpret-as=\"cardinal\">"+ganhadores+
+        "</say-as>"+apostasTexto+" com valor de "+premio+"</speak>";    
+        }
+}
+//*** Chama API de Loterias ***//
+function getOptions(jogo){
+    var options = {
+    host: 'api.caixa.gov.br',
+    port: 8443,
+    path: '/loterias/v2/resultados/'+jogo+'?concurso=',
+    method: 'GET',
+    rejectUnauthorized: false
+    };
+    return options;
+}
+
+//chama Quina
+function getQuina(result) {
+
+    let concurso = result.resultado.concurso;
+    let ganhadores = result.resultado.ganhadores;
+    let sorteados = result.resultado.resultado.split('-').sort();
     let dataSorteio = formata_data(result.resultado.data);
     let dataProximo = formata_data(result.resultado.DT_PROXIMO_CONCURSO);
     let cabecalho = "<speak>ok <break time=\"1s\"/>, para o concurso "+concurso+" foram sorteados: " +
@@ -124,21 +171,7 @@ function getMegaSena(result) {
         "</say-as>"+apostasTexto+" com valor de "+premio+"</speak>";    
         }
 }
-/*var options = getOptions("Mega-Sena");
-console.log(options);
-/////------------------------------
-console.log("//--------------------");
 
-getLoteria(options, function(err, result) {
-    if(err){
-        throw new Error('Error ao acessar a API: ', err);
-        reject();
-    }
-      
-    console.log(result); 
-});
-///---------------------------------
-*/
 function formataReal(n) {
     var valor = parseFloat(n);
         n =  String(valor.toFixed(2)); 
