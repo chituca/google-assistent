b1cb7318c773ed89159f7ab19219690b341943b4
Washington Medeiros <31902882+chituca@users.noreply.github.com>
2018-05-18 21:31:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post("/caixaWebhook", function(req, res) {
                 retorno = lot.getMegaSena(result);
                 break; 
             case loterias.Quina:      
-                getQuina(result);
+                retorno = lot.getQuina(result);
                 break;
             case loterias.Lotofacil:      
                 getLotofacil(result);
@@ -100,74 +100,8 @@ function getOptions(jogo){
     return options;
 }
 
-//chama Mega-Sena
-/*function getMegaSena(result) {
 
-    let concurso = result.resultado.concurso;
-    let ganhadores = result.resultado.ganhadores;
-    let sorteados = result.resultado.resultado.split('-').sort();
-    let dataSorteio = util.formataData(result.resultado.data);
-    let dataProximo = util.formataData(result.resultado.DT_PROXIMO_CONCURSO);
-    let cabecalho = "<speak>ok <break time=\"1s\"/>, para o concurso "+concurso+" foram sorteados: " +
-    "<say-as interpret-as=\"cardinal\">"+sorteados[0]+"</say-as>,"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[1]+"</say-as>,"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[2]+"</say-as>,"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[3]+"</say-as>,"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[4]+"</say-as> e"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[5]+"</say-as> e";
 
-    if(ganhadores === 0) {
-        var estimativa = util.formataReal(result.resultado.VR_ESTIMATIVA);
-        var acumulado = util.formataReal(result.resultado.valor_acumulado1);
-        retorno = cabecalho+"<break time=\"1s\"/>o prêmio acumulou e a estimativa para o próximo concurso, em "+dataProximo+
-        ", é de "+ estimativa + " <break time=\"1s\"/>, o valor acumulado para o próximo concurso é de "+acumulado+".</speak>";
-    
-        } else {
-        var premio = util.formataReal(result.resultado.valor);
-        var apostasTexto = '';
-            if(ganhadores > 1){
-                apostasTexto = "apostas foram premiadas";
-            } else {
-                apostasTexto = "aposta foi premiada";
-            }
-        retorno = cabecalho+"<break time=\"1s\"/> <say-as interpret-as=\"cardinal\">"+ganhadores+
-        "</say-as>"+apostasTexto+" com valor de "+premio+"</speak>";    
-        }
-}*/
-
-//chama Quina
-function getQuina(result) {
-
-    let concurso = result.resultado.concurso;
-    let ganhadores = result.resultado.ganhadores;
-    let sorteados = result.resultado.resultado.split('-').sort();
-    let dataSorteio = util.formataData(result.resultado.data);
-    let dataProximo = util.formataData(result.resultado.DT_PROXIMO_CONCURSO);
-    let cabecalho = "<speak>tudo bem <break time=\"1s\"/>, para o concurso "+concurso+" foram sorteados: " +
-    "<say-as interpret-as=\"cardinal\">"+sorteados[0]+"</say-as>,"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[1]+"</say-as>,"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[2]+"</say-as>,"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[3]+"</say-as>,"+
-    "<say-as interpret-as=\"cardinal\">"+sorteados[4]+"</say-as> e";
-
-    if(ganhadores === 0) {
-        var estimativa = util.formataReal(result.resultado.VR_ESTIMATIVA);
-        var acumulado = util.formataReal(result.resultado.valor_acumulado1);
-        retorno = cabecalho+"<break time=\"1s\"/>o prêmio acumulou e a estimativa para o próximo concurso, em "+dataProximo+
-        ", é de "+ estimativa + " <break time=\"1s\"/>, o valor acumulado para o próximo concurso é de "+acumulado+".</speak>";
-    
-        } else {
-        var premio = util.formataReal(result.resultado.valor1);
-        var apostasTexto = '';
-            if(ganhadores > 1){
-                apostasTexto = "apostas foram premiadas";
-            } else {
-                apostasTexto = "aposta foi premiada";
-            }
-        retorno = cabecalho+"<break time=\"1s\"/> <say-as interpret-as=\"cardinal\">"+ganhadores+
-        "</say-as>"+apostasTexto+" com valor de "+premio+"</speak>";    
-        }
-}
 
 //chama Lotofacil
 function getLotofacil(result) {
