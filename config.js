a0c5778ae77993ebe10161722234d0727de2b544
Washington Medeiros <31902882+chituca@users.noreply.github.com>
2018-05-22 15:56:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,16 @@ host: host = {
     
         gateway: 'api.caixa.gov.br'
 
-    }
+    },
+
+loterias: loterias = {
+        Mega: 'Mega-Sena',
+        Lotofacil: 'Lotofacil',
+        Quina: 'Quina',
+        Lotomania: 'Lotomania',
+        Timemania: 'Timemania',
+        Dupla: 'Dupla-Sena',
+        Federal: 'Federal'
+    }    
 
 }
