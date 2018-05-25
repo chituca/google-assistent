f3d381672d6de016fb4ba868bf31e95aca941de7
Washington Medeiros <31902882+chituca@users.noreply.github.com>
2018-05-25 05:19:10 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+{
+  "name": "Webhook-caixa-assistente",
+  "description": "Webhook Assitente Virtual Caixa",
+  "repository": "https://github.com",
+  "keywords": ["Caixa", "dialogflow", "natural language"]
+}
