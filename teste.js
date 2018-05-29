9dbbfcf4fe3f9dcb88a57146a1c19b9f5c9c7791
Washington Medeiros <31902882+chituca@users.noreply.github.com>
2018-05-29 14:18:10 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,25 @@
+
+function config(app){
+    //logger.info('config mensagem-router');
+    const express = require('express');
+    const router = express.Router();  
+
+    //router.use(loginMiddleware);
+
+    router.get('/teste', teste);
+
+    //app.use('/msg', router);
+}
+
+async function teste(req, res){
+    try{
+        //let wppReturn = await msgService.enviar(req.body);
+        res.send('Teste Caixa Ok!');
+    }catch(err){
+        console.log(err);
+        res.status(500);
+        res.json(err);
+    }
+}
+
+module.exports = config;
